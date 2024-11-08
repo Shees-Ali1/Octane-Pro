8c90e04354aeb9e9cd5bb7ff9e595bc85d354c8a
sheesali <sheesali1000@gmail.com>
2024-11-08 21:50:26 +0500

    new change

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:get/get.dart';
 import 'package:octane_pro/GetxControllers/AuthController.dart';
 import 'package:octane_pro/GetxControllers/Sale-Controller/SaleSummaryController.dart';
 
+import 'GetxControllers/Sale-Controller/FuelDataController.dart';
 import 'GetxControllers/dataController.dart';
 import 'Screens/Auth/login_page.dart';
 import 'Screens/Auth/splash_screen.dart';
@@ -16,7 +17,8 @@ void main() async {
   await Firebase.initializeApp(); // Initialize Firebase here
   Get.put(Authcontroller());
   Get.put(DataController());
-  Get.put(SaleSummaryController());
+  // Get.put(SaleSummaryController());
+  Get.put(FuelDataController());
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
